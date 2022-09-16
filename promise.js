713448f67e6010304bfdb503cf985bddaafb25cc
bambangslstyo <bambangslstyo@gmail.com>
2022-09-16 14:58:13 +0800

    tantangan promise

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let count = 0;
+
+  const ixx = await promiseTheaterIXX();
+  ixx.forEach((val) => {
+    if (val.hasil == emosi) {
+      count++;
+    }
+  });
+
+  const vgc = await promiseTheaterVGC();
+  vgc.forEach((val) => {
+    if (val.hasil == emosi) {
+      count++;
+    }
+  });
+
+  return count;
+}
+
 
 module.exports = {
   promiseOutput,
